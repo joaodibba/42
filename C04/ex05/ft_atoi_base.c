6bf089b04a6dfdd0b08a48da03b9b3c3dac71a3f
Joao Tomas Alves Costa Miranda <jalves-c@c1r2s4.42lisboa.com>
2022-09-26 17:31:40 +0100

    ate agora

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,37 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalves-c <jalves-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/25 21:52:14 by jalves-c          #+#    #+#             */
-/*   Updated: 2022/09/26 16:35:10 by jalves-c         ###   ########.fr       */
+/*   Created: 2022/09/25 22:47:04 by jalves-c          #+#    #+#             */
+/*   Updated: 2022/09/25 22:48:41 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int	ft_atoi(char *str)
-{
-	int	res;
-	int	sign;
-
-	res = 0;
-	sign = 1;
-	while (*str == 32 || (*str >= 9 && *str <= 13))
-		str++;
-	while (*str == '-' || *str == '+')
-	{
-		if (*str == '-')
-			sign *= -1;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		res = res * 10 + *str - 48;
-		str++;
-	}
-	return (res * sign);
-}
