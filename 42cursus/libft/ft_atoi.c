5afc5833a512bf60ad2385e48d3077d4f8673554
João Miranda <joaomiranda.pessoal@gmail.com>
2022-10-27 17:10:29 +0100

    hj foi podre

---
+++ 42cursus/libft/ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dibba <dibba@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 17:14:46 by dibba             #+#    #+#             */
+/*   Updated: 2022/10/27 17:07:41 by dibba            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+
+	while (str[i] && (str[i] >= 9 && str[i] <= 13 || str[i] == 32))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		sign = str[i++] == '-' ? -1 : 1;
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		res *= 10;
+		res += str[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
+
+int main(int ac, char **av)
+{
+	if (ac <= 2)
+	{
+		printf("%d", ft_atoi(av[1]));
+	}
+}
