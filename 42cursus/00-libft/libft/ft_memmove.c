56a49ca95b7eebb2f44e004e876303f4e44ff89f
João Miranda <joaomiranda.pessoal@gmail.com>
2022-11-10 23:55:44 +0000

    update memmove

--- 42cursus/00-libft/libft/ft_memmove.c
+++ 42cursus/00-libft/libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 18:33:50 by jalves-c          #+#    #+#             */
-/*   Updated: 2022/11/10 23:53:29 by jalves-c         ###   ########.fr       */
+/*   Updated: 2022/11/10 23:54:56 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-
-	i = n;
 	if (!dst && !src)
 		return (dst);
 	else if (dst > src)
-		while (i-- > 0)
-			((char *)dst)[i] = ((char *)src)[i];
+		while (n-- > 0)
+			((char *)dst)[n] = ((char *)src)[n];
 	else
 		ft_memcpy(dst, src, n);
 	return (dst);
