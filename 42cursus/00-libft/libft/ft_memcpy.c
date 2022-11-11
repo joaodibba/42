6b232271c55d387decbf2b0dd60e789651e5910e
João Miranda <joaomiranda.pessoal@gmail.com>
2022-11-11 00:20:06 +0000

    update

--- 42cursus/00-libft/libft/ft_memcpy.c
+++ 42cursus/00-libft/libft/ft_memcpy.c
@@ -6,27 +6,23 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:57:28 by jalves-c          #+#    #+#             */
-/*   Updated: 2022/11/10 21:26:38 by jalves-c         ###   ########.fr       */
+/*   Updated: 2022/11/11 00:19:29 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*d;
-	char	*s;
 
 	i = 0;
-	d = (char *)dest;
-	s = (char *)src;
-	if (!dest && !src)
+	if (!dst && !src)
 		return (NULL);
 	while (i < n)
 	{
-		d[i] = s[i];
+		((char *)dst)[i] = ((char *)src)[i];
 		i++;
 	}
-	return (d);
+	return (dst);
 }
