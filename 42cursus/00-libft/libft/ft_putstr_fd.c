d539d8e8bd305de8d65a232e6123d646139aaeb6
Joao Tomas Alves Costa Miranda <jalves-c@c2r9s5.42lisboa.com>
2022-12-05 16:57:35 +0000

    LIBFT'

--- 42cursus/00-libft/libft/ft_putstr_fd.c
+++ 42cursus/00-libft/libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 20:15:30 by jalves-c          #+#    #+#             */
-/*   Updated: 2022/11/16 17:16:20 by jalves-c         ###   ########.fr       */
+/*   Updated: 2022/12/05 16:54:20 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,3 @@ void	ft_putstr_fd(char *s, int fd)
 	while (*s)
 		ft_putchar_fd(*s++, fd);
 }
-//write(fd, s, ft_strlen(s));
--- 42cursus/00-libft/libft/ft_substr.c
+++ 42cursus/00-libft/libft/ft_substr.c
@@ -5,43 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jalves-c < jalves-c@student.42lisboa.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/14 17:21:26 by jalves-c          #+#    #+#             */
-/*   Updated: 2022/12/05 16:33:46 by jalves-c         ###   ########.fr       */
+/*   Created: 2022/12/05 16:39:46 by jalves-c          #+#    #+#             */
+/*   Updated: 2022/12/05 16:40:09 by jalves-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
-char	*ft_substr(const char *s, unsigned int start, size_t len)
-{
-	char	*str;
-	size_t	i;
-	size_t	j;
-	size_t	strlen;
-
-	if (!s)
-		return (0);
-	strlen = ft_strlen(s);
-	if (strlen < len)
-		str = (char *)malloc(sizeof(char) * (strlen + 1));
-	else if (start > strlen)
-		str = (char *)malloc(1);
-	else
-		str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (0);
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (i >= start && j < len)
-			str[j++] = s[i];
-		i++;
-	}
-	str[j] = 0;
-	return (str);
-}
-*/
 
 char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
